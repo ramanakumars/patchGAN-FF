c21bb75a0b0d97abc90cdfc01fc483e0c12908b7
Ramanakumar Sankar <rsankar@umn.edu>
2022-10-18 12:28:42 -0500

    removing deprecated code

--- patchgan/losses.py
+++ patchgan/losses.py
@@ -1,48 +1,35 @@
 import torch
 from torch import nn
 
+
 def tversky(y_true, y_pred, beta, batch_mean=True):
-    tp = torch.sum(y_true*y_pred, axis=(1,2,3))
-    fn = torch.sum((1. - y_pred)*y_true, axis=(1,2,3))
-    fp = torch.sum(y_pred*(1. - y_true), axis=(1,2,3))
-    #tversky = reduce_mean(tp)/(reduce_mean(tp) + 
-    #                           beta*reduce_mean(fn) + 
-    #                           (1. - beta)*reduce_mean(fp))
-    tversky = tp/\
+    tp = torch.sum(y_true*y_pred, axis=(1, 2, 3))
+    fn = torch.sum((1. - y_pred)*y_true, axis=(1, 2, 3))
+    fp = torch.sum(y_pred*(1. - y_true), axis=(1, 2, 3))
+    tversky = tp /\
         (tp + beta*fn + (1. - beta)*fp)
-    
-    norm = torch.sum(y_true, axis=(1,2,3)) + torch.sum(y_pred, axis=(1,2,3)) - tp
+
     if batch_mean:
         return torch.mean((1. - tversky))
     else:
         return (1. - tversky)
 
+
 def fc_tversky(y_true, y_pred, beta, gamma=0.75, batch_mean=True):
     smooth = 1
-    '''
-    y_true_pos = torch.flatten(y_true)
-    y_pred_pos = torch.flatten(y_pred)
-    true_pos = torch.sum(y_true_pos * y_pred_pos, axis=(1,2,3))
-    false_neg = torch.sum(y_true_pos * (1-y_pred_pos), axis=(1,2,3))
-    false_pos = torch.sum((1-y_true_pos)*y_pred_pos, axis=(1,2,3))
-    
-    answer = (true_pos + smooth)/(true_pos + beta*false_neg + (1-beta)*false_pos + smooth)
-    '''
-    tp = torch.sum(y_true*y_pred, axis=(1,2,3))
-    fn = torch.sum((1. - y_pred)*y_true, axis=(1,2,3))
-    fp = torch.sum(y_pred*(1. - y_true), axis=(1,2,3))
-    #tversky = reduce_mean(tp)/(reduce_mean(tp) + 
-    #                           beta*reduce_mean(fn) + 
-    #                           (1. - beta)*reduce_mean(fp))
-    tversky = (tp+smooth)/\
+    tp = torch.sum(y_true*y_pred, axis=(1, 2, 3))
+    fn = torch.sum((1. - y_pred)*y_true, axis=(1, 2, 3))
+    fp = torch.sum(y_pred*(1. - y_true), axis=(1, 2, 3))
+    tversky = (tp+smooth) /\
         (tp + beta*fn + (1. - beta)*fp + smooth)
 
     focal_tversky_loss = 1 - tversky
-    
+
     if batch_mean:
         return torch.pow(torch.mean(focal_tversky_loss), gamma)
-    else: 
+    else:
         return torch.pow(focal_tversky_loss, gamma)
+
 
 # alias
 generator_loss = fc_tversky
