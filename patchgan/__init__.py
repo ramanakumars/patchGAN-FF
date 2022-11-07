d535db8d45d523050d7ab7242ed45d9c15f06390
Ramanakumar Sankar <rsankar@umn.edu>
2022-11-07 12:54:43 -0600

    finalized model architecture

--- patchgan/__init__.py
+++ patchgan/__init__.py
@@ -1,4 +1,9 @@
-from .unet import *
-from .io import *
-from .losses import *
+from .unet import UnetGenerator, Discriminator
+from .io import DataGenerator, MmapDataGenerator
 from .trainer import Trainer
+
+__all__ = [
+    'UnetGenerator', 'Discriminator',
+    'DataGenerator', 'MmapDataGenerator',
+    'Trainer'
+]
